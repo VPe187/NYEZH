f7baecc9a2ea0dcf4c4acfed81137c7c2dc5c3ac
VPe187 <111885049+VPe187@users.noreply.github.com>
2022-11-24 19:53:02 +0100

    Switchs, Seekbar binded

--- app/src/main/java/com/example/nyezh/MainActivity.kt
+++ app/src/main/java/com/example/nyezh/MainActivity.kt
@@ -1,38 +1,126 @@
 package com.example.nyezh
 
+import android.annotation.SuppressLint
 import android.content.ClipData
 import android.content.ClipboardManager
-import android.content.Context
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnTouchListener
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+    private var isLetters  = false
+    private var isUppercase = false
+    private var isNumbers  = false
+    private var isSpecials = false
+    private var passwordLength = 6;
 
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var editTextPassword : EditText = findViewById(R.id.editTextPassword)
-        var buttonGenerate : Button = findViewById(R.id.buttonGenerate)
+        val editTextPassword : EditText = this.findViewById(R.id.editTextPassword)
+        val buttonGenerate : Button = this.findViewById(R.id.buttonGenerate)
+        val switchLetters : Switch = this.findViewById(R.id.switchLetters)
+        val switchUppercase : Switch = this.findViewById(R.id.switchUppercase)
+        val switchNumbers : Switch = this.findViewById(R.id.switchNumbers)
+        val switchSpecials : Switch = this.findViewById(R.id.switchSpecials)
+        val seekBar : SeekBar = this.findViewById(R.id.seekBarPasswordLength)
+        var isTouched = false
+        editTextPassword.isEnabled = false
+        buttonGenerate.isEnabled = false;
 
-        var switchLetters : Switch = findViewById(R.id.switchLetters)
-        var switchUppercase : Switch = findViewById(R.id.switchUppercase)
-        var switchNumbers : Switch = findViewById(R.id.switchLetters)
-        var switchSpecials : Switch = findViewById(R.id.switchLetters)
+        switchLetters.run {
+            setOnTouchListener(object : OnTouchListener {
+                @SuppressLint("ClickableViewAccessibility")
+                override fun onTouch(view: View?, motionEvent: MotionEvent?): Boolean {
+                    isTouched = true
+                    return false
+                }
+            })
+            setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+                if (isTouched) {
+                    isTouched = false
+                    isLetters = isChecked
+                    buttonGenerate.isEnabled = isLetters || isUppercase || isNumbers || isSpecials
+                }
+            })
+        }
 
-        editTextPassword.isEnabled = false;
+        switchUppercase.run {
+            setOnTouchListener(object : OnTouchListener {
+                @SuppressLint("ClickableViewAccessibility")
+                override fun onTouch(view: View?, motionEvent: MotionEvent?): Boolean {
+                    isTouched = true
+                    return false
+                }
+            })
+            setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+                if (isTouched) {
+                    isTouched = false
+                    isUppercase = isChecked
+                    buttonGenerate.isEnabled = isLetters || isUppercase || isNumbers || isSpecials
+                }
+            })
+        }
+
+        switchNumbers.run {
+            setOnTouchListener(object : OnTouchListener {
+                @SuppressLint("ClickableViewAccessibility")
+                override fun onTouch(view: View?, motionEvent: MotionEvent?): Boolean {
+                    isTouched = true
+                    return false
+                }
+            })
+            setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+                if (isTouched) {
+                    isTouched = false
+                    isNumbers = isChecked
+                    buttonGenerate.isEnabled = isLetters || isUppercase || isNumbers || isSpecials
+                }
+            })
+        }
+
+        switchSpecials.run {
+            setOnTouchListener(object : OnTouchListener {
+                @SuppressLint("ClickableViewAccessibility")
+                override fun onTouch(view: View?, motionEvent: MotionEvent?): Boolean {
+                    isTouched = true
+                    return false
+                }
+            })
+            setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+                if (isTouched) {
+                    isTouched = false
+                    isSpecials = isChecked
+                    buttonGenerate.isEnabled = isLetters || isUppercase || isNumbers || isSpecials
+                }
+            })
+        }
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                passwordLength = i
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
+
 
         buttonGenerate.setOnClickListener {
             val password = Password()
-            val generatedPassword = password.generate(20, isLetters = true, isUppercase = true, isNumbers = true, isSpecial = true)
+            val generatedPassword = password.generate(passwordLength, isLetters, isUppercase, isNumbers, isSpecials)
             editTextPassword.setText(generatedPassword)
-            val clipboard: ClipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clipboard: ClipboardManager = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
             val clip = ClipData.newPlainText("password", generatedPassword)
             clipboard.setPrimaryClip(clip)
             Toast.makeText(applicationContext, "Jelszó vágólapra másolva!", Toast.LENGTH_SHORT).show()
-
         }
     }
 }
